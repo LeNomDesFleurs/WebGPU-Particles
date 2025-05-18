e69c0619bc8460a383061e3cb9f6194bcf7212d9
Thomas Guillory <thomasvguillory@gmail.com>
2025-05-18 10:44:16 +0200

    started adding pixel sorting pipeline to a dedicate fie

--- main.js
+++ main.js
@@ -34,74 +34,7 @@ async function init() {
 
 
     // --------------------------- BIND GROUP
-    const bindGroupLayout = device.createBindGroupLayout({
-        entries: [{
-            binding: 0,
-            visibility: GPUShaderStage.COMPUTE,
-            buffer: {
-                type: 'uniform',
-                hasDynamicOffset: false,
-            },
-        },
-        {
-            binding: 1,
-            visibility: GPUShaderStage.COMPUTE,
-            sampler: {
-                type: 'non-filtering',
-            },
-        },
-        {
-            binding: 2,
-            visibility: GPUShaderStage.COMPUTE,
-            texture: {
-                sampleType: 'unfilterable-float',
-                viewDimension: '2d',
-                multisampled: false,
-            },
-        },
-        {
-            binding: 3,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: {
-                access: "write-only",
-                format: "rgba8unorm",
-                viewDimension: '2d',
-
-            },
-        },
-        //Storage
-        {
-            binding: 4,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: {
-                access: "read-write",
-                format: "r32sint",
-                viewDimension: '2d',
-
-            },
-        },
-        {
-            binding: 5,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: {
-                access: "read-write",
-                format: "r32float",
-                viewDimension: '2d',
-
-            },
-        },
-        {
-            binding: 6,
-            visibility: GPUShaderStage.COMPUTE,
-            storageTexture: {
-                access: "read-write",
-                format: "r32uint",
-                viewDimension: '2d',
-
-            },
-        },
-        ],
-    });
+    
 
 
     // must be at least 80 bytes
@@ -224,22 +157,7 @@ async function init() {
         },
     })
 
-    // const createsortvaluePipeline = device.createComputePipeline({
-    //     label: 'tuto',
-    //     layout: pipelineLayout,
-    //     compute: {
-    //         module: sortingModule,
-    //         entryPoint: "CS_CreateSortValues",
-    //     },
-    // })
-
-    // const clearbufferPipeline = device.createComputePipeline({
-    //     label: 'tuto',
-    //     layout: pipelineLayout,
-    //     compute: { module: sortingModule, entryPoint: "CS_ClearBuffers" },
-    // })
-
-
+    
     const identifyspanPipeline = device.createComputePipeline({
         label: 'tuto',
         layout: pipelineLayout,
@@ -263,90 +181,7 @@ async function init() {
         compute: { module: sortingModule, entryPoint: "CS_VisualizeSpans" },
     })
 
-    function draw(p = 0.0, min = 0.0, max = 1.0) {
-
-        const outputTexture = context.getCurrentTexture();
-        const bindGroup = device.createBindGroup({
-            label: 'tuto',
-            layout: bindGroupLayout,
-            entries: [
-                { binding: 0, resource: { buffer: uniformBuffer, } },
-                { binding: 1, resource: sampler },
-                { binding: 2, resource: inputTexture.createView() },
-                { binding: 3, resource: outputTexture.createView() },
-                { binding: 4, resource: storageMask.createView() },
-                { binding: 5, resource: storageSortValue.createView() },
-                { binding: 6, resource: storageSpanLenght.createView() },
-            ],
-        })
-
-        _LowThresholdValue.set([min]);
-        _HighThresholdValue.set([max]);
-        _InvertMaskValue.set([0]);
-        _MaskRandomOffsetValue.set([0.0]);
-        _AnimationSpeedValue.set([0.0]);
-        // max 1000, else, got to change the cache size in the shader
-        _SpanLimitValue.set([1000]);
-        _MaxRandomOffsetValue.set([1]);
-        _SortByValue.set([0]);
-        _ReverseSortingValue.set([0]);
-        _SortedGammaValue.set([1.0]);
-        _FrameTimeValue.set([0.0]);
-        BUFFER_WIDTHValue.set([canvas.width]);
-        BUFFER_HEIGHTValue.set([canvas.height]);
-
-
-        // let angle = p * 180.
-        // let radians = angle * (Math.PI / 180.);
-
-        // const rotationMatrix = mat3.rotation(radians);
-        //Goal is to rescale the rectangle once rotated to avoid cropping
-        // const scale_value = mat3.rotationScale(radians, canvas.width, canvas.height);
-
-        // const scalingMatrix = mat3.scaling([scale_value, scale_value]);
-        // const matrix = mat3.multiply(rotationMatrix, scalingMatrix);
-        // matrixValue.set([
-        //     ...matrix.slice(0, 3), 0,
-        //     ...matrix.slice(3, 6), 0,
-        //     ...matrix.slice(6, 9), 0,
-        // ]);
-        // resolutionValue.set([canvas.width, canvas.height]);
-
-        const encoder = device.createCommandEncoder({ label: 'tuto' })
-        device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
-        device.queue.copyExternalImageToTexture(
-            { source: source },
-            { texture: inputTexture },
-            { width: source.width, height: source.height },
-        );
-
-        const pass = encoder.beginComputePass();
-
-        // let width = canvas.width;
-        let width = 1000;
-        // let height = canvas.height;
-        let height = 1000;
-
-        pass.setBindGroup(0, bindGroup)
-        pass.setPipeline(createMaskPipeline)
-        pass.dispatchWorkgroups(width / 8, height / 8);
-        // pass.setPipeline(createsortvaluePipeline)
-        // pass.dispatchWorkgroups(width / 8, height / 8);
-        // pass.setPipeline(clearbufferPipeline)
-        // pass.dispatchWorkgroups(width / 8, height / 8);
-        pass.setPipeline(identifyspanPipeline)
-        pass.dispatchWorkgroups(width/8, height/8);
-        // pass.setPipeline(visualizePipeline)
-        // pass.dispatchWorkgroups(width, height);
-        pass.setPipeline(pixelsortPipeline)
-        pass.dispatchWorkgroups(width/8, height/8);
-        // pass.setPipeline(compositePipeline)
-        // pass.dispatchWorkgroups(width / 8, height / 8);
-        pass.end()
-
-        const commandBuffer = encoder.finish()
-        device.queue.submit([commandBuffer])
-    }
+   
 
     const rgbSliders = ['control-p', 'min', 'max', ].map((id) =>
         document.getElementById(id)
@@ -364,11 +199,6 @@ async function init() {
     rgbSliders.forEach((slider) => {
         slider.addEventListener('input', handleSliderChange)
     })
-
-//  const rot = document.getElementById('control-p');
-//     rot.addEventListener('input', () => {
-//         draw(parseFloat(rot.value) / 255.0);
-//     })
 
    
     draw()
